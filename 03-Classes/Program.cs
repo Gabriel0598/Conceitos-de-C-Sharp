0fd798a55528c7ae8c932f1dd86c6335e7ff71ce
Gabriel <augustogabriel35@gmail.com>
2021-09-23 01:12:43 -0300

    C4 - Instanciacao e manipulacao de Objetos - Ponto 2D e Ponto 3D

---
+++ 03-Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Classes.Herança;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Ponto p1 = new Ponto(10, 20);
+            //Instanciando ponto num plano 2D;
+            //Possui eixos x e y;
+            //p1 está armazenado no heap, esta é apenas uma referência;
+            //No caso de structs elas acessam o stack diretamente;
+
+            Ponto p2 = new Ponto3D(10, 20, 30);
+            //Instanciando ponto num plano 3D;
+            //Possui eixos x, y e z;
+
+            Ponto3D.Calcular();
+            //Método estático pertence a classe e não a instância, portanto para chamá - lo é necessário chamar a classe primeiro;
+        }
+    }
+}
